bb1d221ec56256ee31a398742aafa68aa5033f0e
Shreyansh Dixit <shreyansh.dixit@gmail.com>
2024-06-10 15:31:32 -0500

    created main page with dummy data

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+let array = [
+    {
+        type: "saving",
+        amount: 500,
+        accountNumber: 1234
+    },
+    {
+        type: "checking",
+        amount: 100,
+        accountNumber: 1248
+    },
+    {
+        type: "checking",
+        amount: 120,
+        accountNumber: 1253
+    }, 
+]
 
 function Main() {
   return (
-    <div>Main</div>
+    //bg-white
+    //bg-blue-600
+    <div className='bg-gray-100  h-screen flex flex-col items-center justify-center text-2xl'>
+        <h1 className='self-start ml-[12.5%] mb-2 font-semibold text-blue-900 text-3xl'>Your Accounts</h1> 
+        <ul className='bg-white shadow-md rounded flex-rows w-[75%] divide-y divide-gray-200'>
+            {array.map((account, index) => {
+            return (
+            <div key={index} className='flex justify-between items-center hover:bg-gray-100 p-2 transition-colors duration-200'>
+                <div className='flex m-2'>
+                    <li className='mx-2'>{account.type}</li>
+                    <li>...{account.accountNumber}</li>
+                </div>
+                <li className='mr-2'>$ {account.amount}</li>
+            </div>)
+           })}
+        </ul>
+    </div>
   )
 }
 
--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -3,19 +3,23 @@ import Logo from "./citiLogo.png"
 
 function NavBar() {
   return (
-    <div className='flex justify-between'>
-      <img
-        className="w-32 h-auto pl-4 sm: pl-8 md: pl-12 lg: pl-16 my-2 py-2"
-        src={Logo}
-        alt='logo'
-      />
-      <div className='w-auto mr-4 p-2 flex items-center justify-between'>
-        <p className='mr-8 text-blue-900 font-semibold sm:text-2xl md:text-2xl lg:text-3xl'>
-          Welcome, Name
-        </p>
-        <button className='text-white bg-blue-900 border-solid border-blue-900 pl-2 pr-2 border-2 text-center rounded-lg hover:bg-blue-800 sm:text-2xl md:text-2xl lg:text-3xl'>
-          Sign Out
-        </button>
+    //bg-blue-600
+    //bg-white
+    <div className="fixed top-0 left-0 w-full bg-white-500 shadow-md" >
+      <div className='flex justify-between'>
+        <img
+          className="w-32 h-auto pl-4 sm: pl-8 md: pl-12 lg: pl-16 my-2 py-2"
+          src={Logo}
+          alt='logo'
+        />
+        <div className='w-auto mr-4 p-2 flex items-center justify-between'>
+          <p className='mr-8 text-blue-900 font-semibold sm:text-2xl md:text-2xl lg:text-3xl'>
+            Welcome, Name
+          </p>
+          <button className='text-white bg-blue-900 border-solid border-blue-800 pl-2 pr-2 border-2 text-center rounded-lg hover:bg-blue-600 sm:text-2xl md:text-2xl lg:text-3xl'>
+            Sign Out
+          </button>
+        </div>
       </div>
     </div>
   )
