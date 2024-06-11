75ddec4050943ebf8b41d33b716f9ac5591d52ad
Keeanu Rodriguez <keeanu7@gmail.com>
2024-06-11 13:34:56 -0400

    added logo functionality

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from "./citiLogo.png"
+import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
@@ -7,11 +8,13 @@ function NavBar() {
     //bg-white
     <div className="fixed top-0 left-0 w-full bg-white-500 shadow-md" >
       <div className='flex justify-between'>
-        <img
-          className="w-32 h-auto pl-4 sm: pl-8 md: pl-12 lg: pl-16 my-2 py-2"
-          src={Logo}
-          alt='logo'
-        />
+        <Link to="/">
+          <img
+            className="w-32 h-auto pl-4 sm: pl-8 md: pl-12 lg: pl-16 my-2 py-2"
+            src={Logo}
+            alt='logo'
+          />
+        </Link>
         <div className='w-auto mr-4 p-2 flex items-center justify-between'>
           <p className='mr-8 text-blue-900 font-semibold sm:text-2xl md:text-2xl lg:text-3xl'>
             Welcome, Name
